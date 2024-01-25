119834eabcb72bb324bd072db7eeed32b3480fa9
Juliano Sirtori <julianosirtori@gmail.com>
2024-01-24 21:13:07 -0300

    workaround

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -167,7 +167,7 @@ export default function Page() {
           </div>
         </Section>
 
-        <Section className="print-force-new-page scroll-mb-16">
+        <Section className=" scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
@@ -180,7 +180,22 @@ export default function Page() {
                   link={"link" in project ? project.link.href : undefined}
                 />
               );
-            })}
+            }).filter((_, index) => index < 3)}
+          </div>
+          <div className="print-force-new-page w-full">
+            <div className="grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3 ">
+              {RESUME_DATA.projects.map((project) => {
+                return (
+                  <ProjectCard
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    tags={project.techStack}
+                    link={"link" in project ? project.link.href : undefined}
+                  />
+                );
+              }).filter((_, index) => index < 3)}
+            </div>
           </div>
         </Section>
       </section>
