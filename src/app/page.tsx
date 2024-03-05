1ea45d0e7394ef1e1ba674b81b607e3d9ae7be71
Juliano Sirtori <julianosirtori@gmail.com>
2024-03-04 21:13:28 -0300

    style: updated style when printed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ export default function Page() {
             );
           })}
         </Section>
-        <Section>
+        <Section className="print-force-new-page">
           <h2 className="text-xl font-bold">Skills</h2>
           <div className="flex flex-wrap gap-1">
             {RESUME_DATA.skills.map((skill) => {
@@ -167,7 +167,7 @@ export default function Page() {
           </div>
         </Section>
 
-        <Section className="print-force-new-page scroll-mb-16">
+        <Section className=" scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
